f7a331fd214d629bddb24fa8104d4240a319e583
udit2720001 <udit2720001@gmail.com>
2024-08-13 21:59:46 +0530

    led with uart

--- src/main.c
+++ src/main.c
@@ -3,27 +3,35 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#include "hardware/gpio.h"
 
 void led_task()
-{   
+{
     stdio_init_all();
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
+    while (true)
+    {
         gpio_put(LED_PIN, 1);
-        vTaskDelay(100);
+        printf("LED ON\n");
+        vTaskDelay(200);
         gpio_put(LED_PIN, 0);
-        vTaskDelay(100);
+        printf("LED OFF\n");
+        vTaskDelay(200);
     }
 }
+
 
 int main()
 {
     stdio_init_all();
 
     xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
+    
     vTaskStartScheduler();
 
-    while(1){};
+    while (1)
+    {
+    };
 }
